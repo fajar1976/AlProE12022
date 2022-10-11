7cd8a6fdd639f499bfa6ba3c7592b75d14219ff5
fajarpahmi <fajarpahmi.2021@student.uny.ac.id>
2022-10-11 11:29:19 +0700

    logika aritmatika

--- Pertemuan2/aritmatika.java
+++ Pertemuan2/aritmatika.java
@@ -1,16 +1,33 @@
-import java.util.scanner;
+import java.util.Scanner;
 
     class aritmatika{
         public static void main(String[] args){
             int x,y,hasil;
 
-            scanner input = new scanner(System.in);
+            Scanner input = new Scanner(System.in);
             System.out.print("Masukan nilai x : ");
             x = input.nextInt();
             System.out.print("Masukab nilai y : ");
             y = input.nextInt();
 
+                //operasi penjumlahan
                 hasil = x + y ;
-                System.out.printIn("Hasil Penjumlahan " +x+ " + "+y" = ");
+                System.out.println("Hasil Penjumlahan " +x+ " + "+y+" = "+hasil); 
+                
+                //operasi Pengurangan 
+                hasil = x - y ;
+                System.out.println("Hasil Penjumlahan " +x+ " - "+y+" = "+hasil);
+                
+                //operasi perkalian
+                hasil = x * y ;
+                System.out.println("Hasil Penjumlahan " +x+ " * "+y+" = "+hasil);
+                
+                //operasi pembagian
+                hasil = x / y ;
+                System.out.println("Hasil Penjumlahan " +x+ " / "+y+" = "+hasil);
+
+                //operasi modulus
+                hasil = x % y ;
+                System.out.println("Hasil Penjumlahan " +x+ " % "+y+" = "+hasil);
         }
     }
